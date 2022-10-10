57787f8247f1899c6a2b6bf962860d8f706dd70c
Luca <luca.camillo@voxxel.com.br>
2022-10-10 11:12:53 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -38,6 +38,9 @@ public class Board {
 	}
 
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There s already a piece on position " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
@@ -51,6 +54,9 @@ public class Board {
 	}
 
 	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return piece(position) != null;
 	}
 }
